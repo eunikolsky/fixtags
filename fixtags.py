387271518916c9ef54e24b7e56022f575a1a96b0
pluton <pluton.od@gmail.com>
2011-02-23 20:16:03 +0200

    Fix for 'Intelligence Squared'

--- fixtags.py
+++ fixtags.py
@@ -305,6 +305,13 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write()
 
+    elif channel_title == 'NPR: Intelligence Squared Podcast':
+        # fix some v2 tags and remove v1
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = 'Intelligence Squared'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write()
+
     elif channel_title == 'Software Engineering Radio':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
