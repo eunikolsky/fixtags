49e8bc0fb8214bf41e63e291a7e1765efdda9e23
pluton <pluton.od@gmail.com>
2011-03-08 20:50:40 +0200

    Fix for 'Healthcare IT Podcast'

--- fixtags.py
+++ fixtags.py
@@ -419,6 +419,16 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2['COM'] = []
         tag2.write()
 
+    elif channel_title == 'Healthcare IT Podcast':
+        # fix some v2 tags and remove v1
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.album = channel_title
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write()
+
     elif channel_title == 'Manager Tools':
         # remove picture element from v2.2 and fix date
         tag2 = stagger.read_tag(episode_fname)
