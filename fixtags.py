74cc6a7ee0bd6addd00418c0bdf98d60eb633fd9
pluton <pluton.od@gmail.com>
2011-03-15 21:00:19 +0200

    Fix for 'ЗаБугром Podcast'

--- fixtags.py
+++ fixtags.py
@@ -297,6 +297,16 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'ЗаБугром Podcast':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.artist = 'Costya (aka sway)'
+        tag2.album = channel_title
+        tag2.genre = 'Podcast'
+        tag2['COM'] = []
+        tag2.write()
+
     elif channel_title == 'NPR: Car Talk Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
