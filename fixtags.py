a26111ae81bf9a574a2ca1ddd1fc87f4ce0e30dd
pluton <pluton.od@gmail.com>
2011-03-18 21:08:57 +0200

    Fix for 'сегодня четверг - dugwin'

--- fixtags.py
+++ fixtags.py
@@ -354,6 +354,15 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write()
 
+    elif channel_title == 'сегодня четверг - dugwin':
+        # fix some v2 tags and remove v1
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.artist = 'dugwin'
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write()
+
     elif channel_title == 'Software Engineering Radio':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
