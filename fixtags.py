6416effe81e1e96b0b577662dadb556aaf37b462
pluton <pluton.od@gmail.com>
2011-04-09 09:57:48 +0300

    Fix for 'Плёнки'

--- fixtags.py
+++ fixtags.py
@@ -480,6 +480,12 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.date = episode_year
         tag2.write()
 
+    elif channel_title == 'Плёнки':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Healthcare IT Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
