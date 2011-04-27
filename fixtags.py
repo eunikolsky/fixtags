acb80f08b9b650f3caf942bcb4651a73a41fb677
pluton <pluton.od@gmail.com>
2011-04-27 22:28:29 +0300

    Fix date for 'Escape from Cubicle Nation Podcast'

--- fixtags.py
+++ fixtags.py
@@ -82,12 +82,11 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
 
     elif channel_title == 'Escape from Cubicle Nation Podcast':
         # set all v2 tags and remove v1
-        tag = stagger.id3v1.Tag1.read(episode_fname)
         tag2 = stagger.Tag24()
         tag2.title = episode_title
         tag2.artist = 'Pamela Slim'
         tag2.album = channel_title
-        tag2.date = tag.year
+        tag2.date = episode_year
         tag2.genre = 'Podcast'
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write(episode_fname)
