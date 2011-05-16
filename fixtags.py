614538e2c40751733229cefdbc62a44b0c549af0
pluton <pluton.od@gmail.com>
2011-05-16 22:16:31 +0300

    Add fix for 'FONAREV - подкаст'

--- fixtags.py
+++ fixtags.py
@@ -532,6 +532,13 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'FONAREV - подкаст':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = channel_title
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Healthcare IT Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
