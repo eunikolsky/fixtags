375484c7de2fd7fb7ff78d1946c25ae2a2243e10
pluton <pluton.od@gmail.com>
2011-06-16 21:48:18 +0300

    Add fix for 'Подкаст 42'

--- fixtags.py
+++ fixtags.py
@@ -565,6 +565,16 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write()
 
+    elif channel_title == 'Подкаст 42':
+        # fix some v2 tags and remove v1
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.album = channel_title
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write()
+
     elif channel_title == 'Manager Tools':
         # remove picture element from v2.2 and fix date
         tag2 = stagger.read_tag(episode_fname)
