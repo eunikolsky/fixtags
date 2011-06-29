7e2a7c9698d04483a24f917e1e6625081b44fb0f
pluton <pluton.od@gmail.com>
2011-06-29 22:01:15 +0300

    Add fix for 'Wordmaster'

--- fixtags.py
+++ fixtags.py
@@ -134,6 +134,16 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write(episode_fname)
 
+    elif channel_title == 'Wordmaster':
+        # set all v2 tags
+        tag2 = stagger.Tag24()
+        tag2.title = episode_title[12:]
+        tag2.artist = 'VOA'
+        tag2.album = channel_title
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        tag2.write(episode_fname)
+
     elif channel_title == 'Science Friday':
         # set all v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
