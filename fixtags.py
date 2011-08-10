4b8f7a6f4869eee7ace408c4c432e2e4bf428254
pluton <pluton.od@gmail.com>
2011-08-10 22:31:20 +0300

    Alter the fix for 'Stuff Mom Never Told You'

--- fixtags.py
+++ fixtags.py
@@ -245,6 +245,7 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
     elif channel_title == 'Stuff Mom Never Told You':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
         tag2.artist = 'Cristen and Molly'
         tag2.album = channel_title
         tag2.genre = 'Podcast'
