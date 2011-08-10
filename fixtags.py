9cf75ec4e93819dbf0273fd8ba84c185ac1c76f1
pluton <pluton.od@gmail.com>
2011-08-10 22:30:28 +0300

    Alter the fix for 'Янки после пьянки'

--- fixtags.py
+++ fixtags.py
@@ -226,6 +226,7 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
     elif channel_title == 'Янки после пьянки':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
         tag2.artist = 'Янки после пьянки'
         tag2.album = channel_title
         tag2.date = episode_year
