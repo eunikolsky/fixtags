cd406c6ec365e14b00a7e5fc864943306ce7aa56
pluton <pluton.od@gmail.com>
2011-08-10 22:32:36 +0300

    Update the fix for 'www.it4business.ru'

--- fixtags.py
+++ fixtags.py
@@ -547,7 +547,11 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
     elif channel_title == 'www.it4business.ru':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.artist = 'Слава Панкратов'
+        tag2.album = channel_title
         tag2.genre = 'Podcast'
+        tag2.date = episode_year
         tag2.write()
 
     elif channel_title == 'Fonarev':
