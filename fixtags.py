22887f933e0756015a08b397dcc6c3695f83f908
pluton <pluton.od@gmail.com>
2011-08-12 20:58:28 +0300

    Add fix for "WNYC's Radiolab"

--- fixtags.py
+++ fixtags.py
@@ -314,6 +314,13 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.album = channel_title
         tag2.write()
 
+    elif channel_title == 'WNYC\'s Radiolab':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = channel_title
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Material World':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
