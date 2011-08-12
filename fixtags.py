fdc7b88ff9647f0260f6555c2f3910107aec1074
pluton <pluton.od@gmail.com>
2011-08-12 20:58:01 +0300

    Add fix for 'Wide Teams'

--- fixtags.py
+++ fixtags.py
@@ -308,6 +308,12 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Wide Teams':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = channel_title
+        tag2.write()
+
     elif channel_title == 'Material World':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
