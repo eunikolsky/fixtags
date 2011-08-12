4da5e66ae4987933ebc096f9819c259c808946e8
pluton <pluton.od@gmail.com>
2011-08-12 20:58:58 +0300

    Add fix for 'NPR: Planet Money'

--- fixtags.py
+++ fixtags.py
@@ -451,6 +451,17 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write()
 
+    elif channel_title == 'NPR: Planet Money':
+        # fix some v2 tags and remove v1
+        tag2 = stagger.Tag24()
+        tag2.title = episode_title
+        tag2.artist = 'Robert Smith'
+        tag2.album = 'Planet Money'
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write(episode_fname)
+
     elif channel_title == 'Software Engineering Radio':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
