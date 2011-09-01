979f566f225910da9cb82b3aa0fe0358f27d4efb
pluton <pluton.od@gmail.com>
2011-09-01 20:53:12 +0300

    Add fix for 'Rosnovsky'

--- fixtags.py
+++ fixtags.py
@@ -212,6 +212,15 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Rosnovsky':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.album = channel_title
+        tag2.genre = 'Podcast'
+        tag2.comment = ''
+        tag2.write()
+
     elif channel_title == 'Freakonomics Radio':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
