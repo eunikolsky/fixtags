0d23d3a8ece773c3c118a23caac50c95de2da949
pluton <pluton.od@gmail.com>
2011-10-11 20:41:27 +0300

    Add fix for 'Казах в Канаде'

--- fixtags.py
+++ fixtags.py
@@ -623,6 +623,13 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Казах в Канаде':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = channel_title
+        tag2.title = episode_title
+        tag2.write()
+
     elif channel_title == 'The English We Speak':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
