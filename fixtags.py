605a870a15a1e95a4ecb5058814eb8792dc5a380
pluton <pluton.od@gmail.com>
2011-11-13 21:04:29 +0200

    Fix title tag of 'Канадский Лось'

--- fixtags.py
+++ fixtags.py
@@ -437,7 +437,7 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
     elif channel_title == 'Канадский Лось':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
-        title = episode_title[42:-1]
+        title = episode_title[41:]
         if title == '':
             title = episode_title
         tag2.title = title
