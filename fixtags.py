b1d2e9d8aa5129c65ca7162ab50f8fe93fc753b3
pluton <pluton.od@gmail.com>
2012-01-30 21:46:50 +0200

    Add fix for 'Dolce Welle - подкаст из Европы'

--- fixtags.py
+++ fixtags.py
@@ -669,6 +669,15 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Dolce Welle - подкаст из Европы':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.artist = 'Alex'
+        tag2.album = channel_title
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Healthcare IT Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
