d67505c00a567a93941abfb372e57b023a7bfff0
pluton <pluton.od@gmail.com>
2012-06-01 22:46:27 +0300

    Add fixes for 'dugwin j. goines // podcast'

--- fixtags.py
+++ fixtags.py
@@ -460,7 +460,8 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write()
 
-    elif channel_title == 'сегодня четверг - dugwin':
+    elif ((channel_title == 'сегодня четверг - dugwin') or
+            (channel_title == 'dugwin j. goines // podcast')):
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
         tag2.artist = 'dugwin'
