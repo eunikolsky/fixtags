cbf46221b7aae9cb8c126c5b36abdc956e27a763
pluton <pluton.od@gmail.com>
2012-06-02 23:41:00 +0300

    Add fixes for 'The Adam Carolla Show'

--- fixtags.py
+++ fixtags.py
@@ -502,6 +502,14 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write(episode_fname)
 
+    elif channel_title == 'The Adam Carolla Show':
+        # fix some v2 tags and remove v1
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = channel_title
+        tag2.genre = 'Podcast'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write(episode_fname)
+
     elif channel_title == 'Software Engineering Radio':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
