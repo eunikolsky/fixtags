38883d6a397ec18891198fd90b9ca6aefe0fbae9
pluton <pluton.od@gmail.com>
2012-09-08 20:38:51 +0300

    Add fixes for 'Начинающий инвестор'

--- fixtags.py
+++ fixtags.py
@@ -94,6 +94,17 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write(episode_fname)
 
+    elif channel_title == 'Начинающий инвестор':
+        # set all v2 tags and remove v1
+        tag2 = stagger.Tag24()
+        tag2.title = episode_title
+        tag2.artist = 'Дмитрий Дмитриев'
+        tag2.album = channel_title
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        stagger.id3v1.Tag1.delete(episode_fname)
+        tag2.write(episode_fname)
+
     elif channel_title == 'EconTalk':
         # set all v2 tags
         tag2 = stagger.Tag24()
