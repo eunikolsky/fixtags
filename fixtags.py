9f0107f47c5a1b31da20e031d38dca2e240ede04
pluton <pluton.od@gmail.com>
2013-01-22 23:07:30 +0200

    Add fixes for 'Откровенно про IT-карьеризм'

--- fixtags.py
+++ fixtags.py
@@ -805,6 +805,10 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         stagger.id3v1.Tag1.delete(episode_fname)
         tag2.write()
 
+    elif channel_title == 'Откровенно про IT-карьеризм':
+        # remove v1
+        stagger.id3v1.Tag1.delete(episode_fname)
+
     elif channel_title == 'Manager Tools':
         # remove picture element from v2.2 and fix date
         tag2 = stagger.read_tag(episode_fname)
