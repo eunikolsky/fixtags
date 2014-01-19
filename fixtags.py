affed24d601a5286f38da429d7be8da159156e15
pluton <pluton.od@gmail.com>
2014-01-19 16:27:51 +0200

    Add fixes for 'The Haskell Cast'

--- fixtags.py
+++ fixtags.py
@@ -837,6 +837,12 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'The Haskell Cast':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Healthcare IT Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
