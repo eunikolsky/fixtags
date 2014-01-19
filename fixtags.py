2e9f11afb4a80b14a53a3b5f49d62da4dacf5460
pluton <pluton.od@gmail.com>
2014-01-19 20:14:49 +0200

    Set title for 'The Haskell Cast'

--- fixtags.py
+++ fixtags.py
@@ -840,6 +840,7 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
     elif channel_title == 'The Haskell Cast':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
         tag2.genre = 'Podcast'
         tag2.write()
 
