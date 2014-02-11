f94fc4eab63d75e2d28264b599dfd0577cb240fb
pluton <pluton.od@gmail.com>
2014-02-11 21:46:41 +0200

    Rename 'Rosnovsky in Canada' to 'Evergreen'

--- fixtags.py
+++ fixtags.py
@@ -293,7 +293,7 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
-    elif channel_title == 'Rosnovsky in Canada':
+    elif channel_title == 'Evergreen':
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
         tag2.title = episode_title
