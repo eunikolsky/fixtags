0176b83392f0abe02e025c69611ed10b08aa38a7
pluton <pluton.od@gmail.com>
2014-04-27 16:12:58 +0300

    Create tag if doesn't exist for 'happy friday podcast from gAmUssA ;-)'

--- fixtags.py
+++ fixtags.py
@@ -437,10 +437,16 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
 
     elif channel_title == 'happy friday podcast from gAmUssA ;-)':
         # fix some v2 tags
-        tag2 = stagger.read_tag(episode_fname)
+        try:
+            tag2 = stagger.read_tag(episode_fname)
+        except stagger.errors.NoTagError:
+            tag2 = stagger.Tag24()
+        tag2.artist = 'gAmUssA'
         tag2.title = episode_title
         tag2.album = channel_title
-        tag2.write()
+        tag2.genre = 'Podcast'
+        tag2.date = episode_year
+        tag2.write(episode_fname)
 
     elif channel_title == 'Material World':
         # fix some v2 tags and remove v1
