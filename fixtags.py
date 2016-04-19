e98daadcad746400f4a3a5f50b3541240c0fa480
pluton <pluton.od@gmail.com>
2016-04-18 18:36:00 -0700

    'Under the Radar': add fixes

--- fixtags.py
+++ fixtags.py
@@ -902,6 +902,12 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Under the Radar':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Healthcare IT Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
