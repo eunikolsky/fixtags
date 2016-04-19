444199ba444c7de5a878ad1ce52646e3bde8ce6b
pluton <pluton.od@gmail.com>
2016-04-18 18:34:59 -0700

    'Разбор Полетов': use full episode title

--- fixtags.py
+++ fixtags.py
@@ -606,7 +606,7 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
     elif channel_title == 'Разбор Полетов':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
-        tag2.title = episode_title[13:]
+        tag2.title = episode_title
         tag2.date = episode_year
         tag2.album = channel_title
         tag2.genre = 'Podcast'
