19dc64555ac8e35568d2d4adcd949ec1db08ae73
pluton <pluton.od@gmail.com>
2017-01-02 18:34:53 -0800

    Add 'Подкаст "На чемоданах"'

--- fixtags.py
+++ fixtags.py
@@ -915,6 +915,16 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Подкаст "На чемоданах"':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.artist = 'degiz'
+        tag2.album = channel_title
+        tag2.date = episode_year
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Healthcare IT Podcast':
         # fix some v2 tags and remove v1
         tag2 = stagger.read_tag(episode_fname)
