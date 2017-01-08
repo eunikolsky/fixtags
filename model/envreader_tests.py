5056078bf952b49694b02761cf286710a6dc2ea8
pluton <pluton.od@gmail.com>
2017-01-07 23:01:55 -0800

    Remove duplication in TestEnvReader_EpisodeFilename

--- model/envreader_tests.py
+++ model/envreader_tests.py
@@ -4,7 +4,16 @@ import envreader
 
 import unittest
 
-class TestEnvReader_EpisodeTitle(unittest.TestCase):
+class _TestEnvReader_Base(unittest.TestCase):
+    '''Base class providing helpers to verify EpisodeInfo.'''
+
+    def assertEqualC(self, x):
+        '''Curried version of `assertEqual`.'''
+        def _assertEqualC(y):
+            self.assertEqual(x, y)
+        return _assertEqualC
+
+class TestEnvReader_EpisodeTitle(_TestEnvReader_Base):
     def test_should_be_set_from_environ(self):
         self._verify_equal_title('anonymousTitle')
 
@@ -28,35 +37,26 @@ class TestEnvReader_EpisodeTitle(unittest.TestCase):
     def _get_fake_environ(self, episode_title):
         return {'GPODDER_EPISODE_TITLE': episode_title}
 
-    def assertEqualC(self, x):
-        '''Curried version of `assertEqual`.'''
-        def _assertEqualC(y):
-            self.assertEqual(x, y)
-        return _assertEqualC
-
-class TestEnvReader_EpisodeFilename(unittest.TestCase):
+class TestEnvReader_EpisodeFilename(_TestEnvReader_Base):
     def test_should_be_set_from_environ(self):
-        anonymousFilename = 'anonymousFilename'
-        fake_environ = self._get_fake_environ(anonymousFilename)
-
-        info = envreader.get_episode_info(fake_environ)
-
-        self.assertEqual(info.episode_filename, anonymousFilename)
+        self._verify_equal_filename('anonymousFilename')
 
     def test_should_be_empty_when_empty(self):
-        emptyFilename = ''
-        fake_environ = self._get_fake_environ(emptyFilename)
-
-        info = envreader.get_episode_info(fake_environ)
-
-        self.assertEqual(info.episode_filename, emptyFilename)
+        self._verify_equal_filename('')
 
     def test_should_be_None_when_missing(self):
-        fake_environ = {}
+        self._verify_filename_env(self.assertIsNone, {})
 
+    def _verify_equal_filename(self, filename):
+        self._verify_filename(self.assertEqualC(filename), filename)
+
+    def _verify_filename(self, assertion, filename):
+        fake_environ = self._get_fake_environ(filename)
+        self._verify_filename_env(assertion, fake_environ)
+
+    def _verify_filename_env(self, assertion, fake_environ):
         info = envreader.get_episode_info(fake_environ)
-
-        self.assertIsNone(info.episode_filename)
+        assertion(info.episode_filename)
 
     def _get_fake_environ(self, episode_title):
         return {'GPODDER_EPISODE_FILENAME': episode_title}
