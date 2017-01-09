fad391aef2c13a0816d479124aa8dde28998c649
pluton <pluton.od@gmail.com>
2017-01-08 19:01:57 -0800

    envreader: get episode_timestamp from environment

--- model/envreader.py
+++ model/envreader.py
@@ -3,7 +3,7 @@
 import collections
 
 EpisodeInfo = collections.namedtuple('EpisodeInfo',
-        ['episode_title', 'episode_filename', 'podcast_title'])
+        ['episode_title', 'episode_filename', 'podcast_title', 'episode_timestamp'])
 '''A tuple containing episode information from gPodder.'''
 
 def get_episode_info(environ):
@@ -15,12 +15,20 @@ def get_episode_info(environ):
             with `os.environ`.
     '''
 
+    def parse_timestamp(key):
+        try:
+            return int(environ[key])
+        except (ValueError, KeyError):
+            return None
+
     KEYS = ['GPODDER_EPISODE_TITLE',
             'GPODDER_EPISODE_FILENAME',
             'GPODDER_CHANNEL_TITLE']
-    return EpisodeInfo(*map(environ.get, KEYS))
+    pubdate = parse_timestamp('GPODDER_EPISODE_PUBDATE')
+    return EpisodeInfo(*map(environ.get, KEYS), pubdate)
 
 # Getter functions
 episode_title = lambda x: x.episode_title
 episode_filename = lambda x: x.episode_filename
 podcast_title = lambda x: x.podcast_title
+episode_timestamp = lambda x: x.episode_timestamp
--- model/envreader_tests.py
+++ model/envreader_tests.py
@@ -85,5 +85,32 @@ class TestEnvReader_PodcastTitle(_TestEnvReader_Base):
     def _get_fake_environ(self, episode_title):
         return {'GPODDER_CHANNEL_TITLE': episode_title}
 
+class TestEnvReader_PublicationTimestamp(_TestEnvReader_Base):
+    def test_should_be_set_as_int_from_environ(self):
+        anonymousTimestamp = 1234567890
+        self._verify_equal_timestamp(anonymousTimestamp, str(anonymousTimestamp))
+
+    def test_should_be_None_when_empty(self):
+        self._verify_timestamp(self.assertIsNone, '')
+
+    def test_should_be_None_when_not_a_number(self):
+        self._verify_timestamp(self.assertIsNone, '123x')
+
+    def test_should_be_None_when_missing(self):
+        self._verify_timestamp_env(self.assertIsNone, {})
+
+    def _verify_equal_timestamp(self, expected, timestamp):
+        self._verify_timestamp(self.assertEqualC(expected), timestamp)
+
+    def _verify_timestamp(self, assertion, timestamp):
+        fake_environ = self._get_fake_environ(timestamp)
+        self._verify_timestamp_env(assertion, fake_environ)
+
+    def _verify_timestamp_env(self, assertion, fake_environ):
+        self.verify_with_env(assertion, envreader.episode_timestamp, fake_environ)
+
+    def _get_fake_environ(self, episode_timestamp):
+        return {'GPODDER_EPISODE_PUBDATE': episode_timestamp}
+
 if __name__ == '__main__':
     unittest.main()
