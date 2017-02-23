fca44634006195c9f032849c9f1ec2297c7f6023
pluton <pluton.od@gmail.com>
2017-02-22 20:36:17 -0800

    [Coffee Break German] Add fixes

--- fixtags.py
+++ fixtags.py
@@ -816,6 +816,13 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.album = channel_title
         tag2.write()
 
+    elif channel_title == 'Coffee Break German':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = episode_title
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == "JavaPubHouse Off-Heap's podcast":
         # fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
