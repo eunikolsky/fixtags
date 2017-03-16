e43dc89c1e0b57fefc1ac90be0f78f40f061951e
pluton <pluton.od@gmail.com>
2017-03-15 19:33:51 -0700

    [Эхо Москвы. Точка] Create tag if not present

--- fixtags.py
+++ fixtags.py
@@ -561,14 +561,17 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
 
     elif channel_title == 'Эхо Москвы. Точка':
         # fix some v2 tags
-        tag2 = stagger.read_tag(episode_fname)
+        try:
+            tag2 = stagger.read_tag(episode_fname)
+        except stagger.errors.NoTagError:
+            tag2 = stagger.Tag24()
         tag2.title = episode_title
         tag2.artist = 'Александр Плющев'
         tag2.composer = 'Эхо Москвы'
         tag2.album = 'Точка'
         tag2.date = episode_year
         tag2.genre = 'Podcast'
-        tag2.write()
+        tag2.write(episode_fname)
 
     elif channel_title == 'Радио-Т' or channel_title == 'Пираты-РТ':
         # fix some v2 tags
