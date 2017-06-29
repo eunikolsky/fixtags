fc76fd639aa7bbcc8e9ddc6068e6182eb5fac419
pluton <pluton.od@gmail.com>
2017-06-28 22:06:50 -0700

    [LambdaCast] Add fixes

--- fixtags.py
+++ fixtags.py
@@ -858,6 +858,13 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'LambdaCast':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.album = channel_title
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Scalalaz Podcast':
         # remove picture element from v2.2 and fix some v2 tags
         tag2 = stagger.read_tag(episode_fname)
