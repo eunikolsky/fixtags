ca72aeaba908b49ded0553b05b44c4af37e4dec1
pluton <pluton.od@gmail.com>
2018-01-28 15:08:48 -0800

    [Command Line Heroes] Add fixes

--- fixtags.py
+++ fixtags.py
@@ -931,6 +931,12 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.title = trim_prefix(episode_title, 'Functional Geekery Episode ')
         tag2.write()
 
+    elif channel_title == 'Command Line Heroes':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Scalalaz Podcast':
         # remove picture element from v2.2 and fix some v2 tags
         try:
