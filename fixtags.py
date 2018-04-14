606817fd41fd73dd9a42befada4e3275cfaf51ff
pluton <pluton.od@gmail.com>
2018-04-14 16:18:42 -0700

    [Top-Thema mit Vokabeln] remove 'APIC' tag

--- fixtags.py
+++ fixtags.py
@@ -1002,6 +1002,15 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'Top-Thema mit Vokabeln | Deutsch lernen | Deutsche Welle':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        try:
+            del tag2['APIC']
+        except KeyError:
+            pass
+        tag2.write()
+
     elif ((channel_title == 'UWP - Eженедельный подкаст от Umputun')
             or (channel_title == 'Discovery')
             or (channel_title == 'Охотник За Головами - Денис aka Radio Grinch')
