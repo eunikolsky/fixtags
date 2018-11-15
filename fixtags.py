e4a62f71138f897e55b584238fbaadf641e9aa6a
pluton <pluton.od@gmail.com>
2018-11-14 17:42:19 -0800

    [This is Love] Add fixes

--- fixtags.py
+++ fixtags.py
@@ -1018,6 +1018,17 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'This is Love':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.title = trim_prefix(episode_title, 'Episode ')
+        tag2.album = channel_title
+        try:
+            del tag2['APIC']
+        except KeyError:
+            pass
+        tag2.write()
+
     elif channel_title == 'Scalalaz Podcast':
         # remove picture element from v2.2 and fix some v2 tags
         try:
