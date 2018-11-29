44898405d02408f0eb4665019fa5e58b00aeb4de
pluton <pluton.od@gmail.com>
2018-11-28 18:25:05 -0800

    [The Art Of Programming] Add fixes

--- fixtags.py
+++ fixtags.py
@@ -1036,6 +1036,12 @@ For more information, go to 'http://wiki.gpodder.org/wiki/User_Manual#Time_stret
         tag2.genre = 'Podcast'
         tag2.write()
 
+    elif channel_title == 'The Art Of Programming':
+        # fix some v2 tags
+        tag2 = stagger.read_tag(episode_fname)
+        tag2.genre = 'Podcast'
+        tag2.write()
+
     elif channel_title == 'Scalalaz Podcast':
         # remove picture element from v2.2 and fix some v2 tags
         try:
